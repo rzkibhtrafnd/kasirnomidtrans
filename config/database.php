5a935013180aeb47c66ee4e89657b2cc2e18d4b3
rzkibhtrafnd <rzkibhtrafnd09@gmail.com>
2026-01-20 10:47:54 +0700

    Remove mysql url config to force DB_HOST

--- config/database.php
+++ config/database.php
@@ -44,7 +44,6 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => null,
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
